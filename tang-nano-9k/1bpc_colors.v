f57268927ebe3632ad92719009ec94a55d8ca51a
Blaž Rojc <blaz.rojc@landisgyr.com>
2024-05-16 15:15:43 +0200

    Unsuccessful attempt at 1BPC colors.

--- tang-nano-9k/1bpc_colors.v
+++ tang-nano-9k/1bpc_colors.v
@@ -1,17 +1,18 @@
 /*
-Drive an LED matrix with a solid white color.
+Drive an LED matrix with changing 1BPC solid color.
 */
 
 `include "includes/prescaler.v"
 
 module top (
     input clk,
-	output H75_R1,
-    output H75_R2,
-    output H75_G1,
-    output H75_G2,
-    output H75_B1,
-    output H75_B2,
+    input S2,
+    output reg H75_R2,
+    output reg H75_R1,
+    output reg H75_G1,
+    output reg H75_G2,
+    output reg H75_B1,
+    output reg H75_B2,
     output H75_A,
     output H75_B,
     output H75_C,
@@ -20,28 +21,50 @@ module top (
     output reg H75_OE,
     output reg H75_Clk,
     output reg H75_Lat,
+	output [5:0] led,
 );
+
+wire color_clk;
+
+// Color should change once per second, so divide the 27MHz external clock by 27'000'000.
+prescaler #(
+    .NUM(27000000),
+    .NUM_BITS(32),
+)
+color_psc (
+    .clk(clk),
+    .rst(!S2),
+    .enable(color_clk),
+);
+
+reg [2:0] color;
+
+assign led[0] = color[0];
+assign led[1] = color[0];
+assign led[2] = color[1];
+assign led[3] = color[1];
+assign led[4] = color[2];
+assign led[5] = color[2];
+
+always @(posedge clk) begin
+    if (color_clk) begin
+        color <= color + 1'b1;
+    end
+end
+
 
 wire pixel_clk;
 
-// 1MHz clock is desired, so divide the 27MHz extrenal clock by 27.
+// 1MHz clock is desired, so divide the 27MHz external clock by 27.
 prescaler #(
     .NUM(27),
-    .NUM_BITS(6),
+    .NUM_BITS(32),
 )
-psc (
+pixel_psc (
     .clk(clk),
     .rst(1'b0),
     .enable(pixel_clk),
 );
-
-// We want an all-white panel.
-assign H75_R1 = 1'b1;
-assign H75_R2 = 1'b1;
-assign H75_G1 = 1'b1;
-assign H75_G2 = 1'b1;
-assign H75_B1 = 1'b1;
-assign H75_B2 = 1'b1;
 
 /*
 The HUB75 protocol works like this:
@@ -72,23 +95,29 @@ always @(posedge clk) begin
             8'b0xxx_xxx1: begin
                 // Set clock low.
                 H75_Clk <= 1'b0;
+                H75_R1 <= color[0];
+                H75_R2 <= color[0];
+                H75_G1 <= color[1];
+                H75_G2 <= color[1];
+                H75_B1 <= color[2];
+                H75_B2 <= color[2];
             end
             8'b1000_0000: begin
-                // Pull Output Enable high.
-                H75_OE <= 1'b1;
-            end
-            8'b1000_0001: begin
                 // Latch high.
                 H75_Lat <= 1'b0;
             end
-            8'b1000_0010: begin
+            8'b1000_0001: begin
                 // Latch low.
                 H75_Lat <= 1'b0;
                 // Update line.
                 line <= line + 1'b1;
             end
-            8'b1000_0011: begin
+            8'b1000_0010: begin
                 // Pull Output Enable low.
+                H75_OE <= 1'b1;
+            end
+            8'b1111_1111: begin
+                // Pull Output Enable high.
                 H75_OE <= 1'b0;
             end
         endcase
